8ce371911cbca82e542ec3c37c1216cd5d2dc1f1
Elijah Jian <hjxtty@outlook.com>
2020-02-13 01:45:25 +0800

    第一次提交

---
+++ TestCSharpLanguage/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestCSharpLanguage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test C# Languages");
+        }
+    }
+}
