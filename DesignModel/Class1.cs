e285507a8cb61ec8178497571a755ab72dd4da45
Elijah Jian <hjxtty@outlook.com>
2020-02-19 14:05:47 +0800

    * Class1.cs: 增加设计模式类
    * TestCSharpLanguage.sln:
    * DesignModel.csproj:

---
+++ DesignModel/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DesignModel
+{
+    public class Class1
+    {
+        /// <summary>
+        /// 23种设计模式
+        /// 创建型：对象创建
+        /// 1、单例模式：
+        /// 结构型：类与类之间的关系
+        /// 行为型：对象和行为的分离
+        /// </summary>
+        public void func()
+        {
+
+        }
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.Read();
+        }
+    }
+}
